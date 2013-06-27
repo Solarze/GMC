30b762344ba4371dd71459df083d49156e45ccae
Solarze <davidjfhearnden@gmail.com>
2013-06-27 06:23:24 +0100

    Bare Bones of the Mod class

---
+++ geno_common/geno/G3N0Core.java
@@ -0,0 +1,31 @@
+package geno;
+
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class G3N0Core
+{
+    public static G3N0Core instance;
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+}
