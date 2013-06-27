f1f0072106de11ddf71fb9300b278602936bd073
Solarze <davidjfhearnden@gmail.com>
2013-06-27 16:49:23 +0100

    Mod File

--- geno_common/geno/G3N0Core.java
+++ geno_common/geno/G3N0Core.java
@@ -1,5 +1,6 @@
 package geno;
 
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
@@ -7,6 +8,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = "GMC", name = "G3N0's Mod Compendium", version = "0.0.1")
 public class G3N0Core
 {
     public static G3N0Core instance;
