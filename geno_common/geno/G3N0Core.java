1ea21d0e85dd147e312394ea126bc4dd5ad58a78
Solarze <davidjfhearnden@gmail.com>
2013-06-27 17:10:55 +0100

    License

--- geno_common/geno/G3N0Core.java
+++ geno_common/geno/G3N0Core.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of G3N0's mod compendium
+ * 
+ * G3N0's mod compendium is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * G3N0's mod compendium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABLILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with G3N0's mod compendium. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package geno;
 
 import cpw.mods.fml.common.Mod;
